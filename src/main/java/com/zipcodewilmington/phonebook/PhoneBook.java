95feb252ac005ee9b4692c67fbc042a9f7bc88d2
Christy <christy@zipcodersmbp90.mynetworksettings.com>
2023-12-07 23:28:10 -0500

    working in PhoneBook methode

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -12,51 +12,46 @@ import java.util.*;
 
 public class PhoneBook {
 
-    public static void main(String[] args) {
-        Map<String,Integer> map  = new HashMap<>();
-
-        map.put("Christ", 610-222-4569);
-        map.put("Joe", 610-544-9087);
-        map.put("Sam", 610-674-7865);
-        map.put("Dean", 610-777-4567);
-    }
-
-
-
     private final Map<String, List<String>> phonebook;
 
-    public Map<String, List<String>> getPhonebook() {
-        return phonebook;
-    }
-
-
-
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook =null;
     }
 
     public PhoneBook() {
-        this(null);
+        this(new LinkedHashMap<String, List<String>>());
     }
 
     public void add(String name, String phoneNumber) {
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
+
     }
 
+
     public void remove(String name) {
+
     }
 
     public Boolean hasEntry(String name) {
+
         return null;
     }
 
-    public List<String> lookup(String name) {
-        return null;
+    public Boolean hasEntry(String name, String phoneNumber) {
+
+       return null;
     }
 
-    public String reverseLookup(String phoneNumber) {
+    public List<String> lookup(String name) { // Looking for the value
+        return null;
+
+    }
+
+
+    public String reverseLookup(String phoneNumber) { // looking the key
         return null;
     }
 
